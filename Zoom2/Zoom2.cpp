cc0d35a7db03d29693c6579595bb104351f6e753
JanOveBicepS <olemr16@gmail.com>
2020-09-01 12:03:35 +0200

    Add project files.

---
+++ Zoom2/Zoom2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+//using namespace std;
+int age = 21;
+bool sw = true;
+char Initial = 'O';
+std::string name = "Ole";
+double pi = 3.14;
+
+int main() {
+	std::cout << "enter your age :";
+	std::cin >> age;
+ std::cout << "Your age is : " << age << "\n";  // = std::endl;  \t
+ std::cout << "You are young "; 
+ std::cout << "s";
+
+}
