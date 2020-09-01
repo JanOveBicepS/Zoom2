d66f1e18980f0281e4ae6fc5b9b39da535b7a9d6
JanOveBicepS <olemr16@gmail.com>
2020-09-01 14:00:30 +0200

    Made some changes

--- Zoom2/Zoom2.cpp
+++ Zoom2/Zoom2.cpp
@@ -6,6 +6,32 @@ bool sw = true;
 char Initial = 'O';
 std::string name = "Ole";
 double pi = 3.14;
+int a = 10;
+int b = 5;
+int c = 20;
+int z = 10;
+
+int main() {
+
+	system("cls");
+	system("pause");
+
+	if (a > b) {
+		std::cout << "a is greater than b\n";
+	}
+	else {
+		std::cout << "b is greater than a\n";
+	}
+	if (a == z) {
+		std::cout << "a and z are equal \n";
+	}
+	if (a >= z) { //if ( a > z && a == z) && is and, || is or, ! is not;
+		std::cout << "nothing happens here \n";
+	}
+	if (a != b) {
+		std::cout << "a and b is not equal\n";
+	}
+}
 
 int main() {
 	std::cout << "enter your age :";
@@ -15,3 +41,6 @@ int main() {
  std::cout << "s";
 
 }
+
+
+//_getch() have to include <conio.h>            std::getline(std::cout
